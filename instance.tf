7239a7d5d677c18d0367a72bd3d4fc6918ab320e
Jignesh <jignesh.9202@gmail.com>
2022-08-27 14:38:44 +0530

    Added startup script

--- instance.tf
+++ instance.tf
@@ -22,7 +22,8 @@ data "aws_subnet" "selected" {
 }
 
 resource "aws_instance" "ec2-s3" {
-  ami = data.aws_ami_ids.ubuntu.ids[0]
+  //ami = data.aws_ami_ids.ubuntu.ids[0]
+  ami = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
   iam_instance_profile = aws_iam_instance_profile.test_profile.name
   subnet_id = data.aws_subnet.selected.id
